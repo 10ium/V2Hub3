1e58d34ddae3b8a44d313cd19791592606c9767e
Tenium <amin.behravan@yahoo.com>
2025-04-16 09:36:44 +0330

    Update config.php

--- config.php
+++ config.php
@@ -4,14 +4,5 @@ $subscription_urls = [
   "https://raw.githubusercontent.com/10ium/V2rayCollectorLite/refs/heads/main/vmess_iran.txt",
   "https://raw.githubusercontent.com/10ium/V2rayCollectorLite/refs/heads/main/ss_iran.txt",
   "https://raw.githubusercontent.com/10ium/V2rayCollectorLite/refs/heads/main/trojan_iran.txt",
-  "https://raw.githubusercontent.com/10ium/V2rayCollectorLite/refs/heads/main/vless_iran.txt",
-  "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mci/sub_1.txt",
-  "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mci/sub_2.txt",
-  "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mci/sub_3.txt",
-  "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mci/sub_4.txt",
-  "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mtn/sub_1.txt",
-  "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mtn/sub_2.txt",
-  "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mtn/sub_3.txt",
-  "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/main/mtn/sub_4.txt",
-  "https://raw.githubusercontent.com/4n0nymou3/multi-proxy-config-fetcher/refs/heads/main/configs/proxy_configs.txt"
+  "https://raw.githubusercontent.com/10ium/V2rayCollectorLite/refs/heads/main/vless_iran.txt"
 ];
